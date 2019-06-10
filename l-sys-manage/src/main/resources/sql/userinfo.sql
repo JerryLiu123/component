56d25349e09a82c6ba10509b57bc81e70068d537
刘国华 <liuguohua@mail.taiji.com.cn>
2019-06-10 15:37:06 +0800

    修改创建库sql

--- l-sys-manage/src/main/resources/sql/userinfo.sql
+++ l-sys-manage/src/main/resources/sql/userinfo.sql
@@ -11,17 +11,17 @@
  Target Server Version : 50725
  File Encoding         : 65001
 
- Date: 29/05/2019 13:43:44
+ Date: 10/06/2019 15:36:11
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for userinfo
+-- Table structure for userinfo_copy1
 -- ----------------------------
-DROP TABLE IF EXISTS `userinfo`;
-CREATE TABLE `userinfo` (
+DROP TABLE IF EXISTS `userinfo_copy1`;
+CREATE TABLE `userinfo_copy1` (
   `id` varchar(255) NOT NULL,
   `create_time` datetime DEFAULT NULL,
   `creator_id` varchar(255) DEFAULT NULL,
@@ -32,14 +32,8 @@ CREATE TABLE `userinfo` (
   `state` varchar(255) DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `updater_id` varchar(255) DEFAULT NULL,
+  `update_pwd_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of userinfo
--- ----------------------------
-BEGIN;
-INSERT INTO `userinfo` VALUES ('12', '2018-11-15 11:06:38', NULL, 1, 'admin', '320c4b482db0b6e123e28270e6109783', NULL, '1', NULL, NULL);
-COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
